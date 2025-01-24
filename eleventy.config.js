6c7ba8e864125637c640b5ba3abde5a8def33495
Chrissy-Barrett <32103897+Chrissy-Barrett@users.noreply.github.com>
2025-01-24 17:12:57 +0000

    Add files via upload
    init

---
+++ eleventy.config.js
@@ -0,0 +1,32 @@
+const govukEleventyPlugin = require('@x-govuk/govuk-eleventy-plugin')
+
+module.exports = function(eleventyConfig) {
+  // Register the plugin
+  eleventyConfig.addPlugin(govukEleventyPlugin, {
+    header: {
+        logotype: {
+          text: 'Sustainable by design'
+        }
+    },
+    titleSuffix: 'Sustainable by design',
+    footer: {
+        copyright: {
+            text: '© Sustainable by design'
+          }
+    }
+});
+
+  return {
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: 'njk',
+    dir: {
+      // The folder where all your content will live:
+      input: 'app',
+      // Use layouts from the plugin
+      includes: '_layouts'
+    },
+    pathPrefix: process.env.GITHUB_ACTIONS ? '/blog-test/' : '/'
+  }
+
+  };
